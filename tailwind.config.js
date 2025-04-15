c0e35c1260cac42a8ad546f6bcdbd868ef34affd
Gagancreates <gaganp000999@gmail.com>
2025-04-16 02:31:22 +0530

    made some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,9 @@ module.exports = {
           "accordion-down": "accordion-down 0.2s ease-out",
           "accordion-up": "accordion-up 0.2s ease-out",
         },
+        borderColor: {
+          border: 'hsl(var(--border))',
+        }
       },
     },
     plugins: [require("tailwindcss-animate")],
